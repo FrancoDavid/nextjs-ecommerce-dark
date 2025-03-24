f4a0598290501f0225df55b9aacb7e60eb0155e3
redrum <franco.avalos@live.com>
2025-03-24 19:48:52 -0300

    feat: header component

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
+export default function Header() {
+    return (
+        <>
+            <header className="bg-black stycky top-0 z-50">
+                <div className="container mx-auto flex justify-around items-center p-2">
+                    <span className="text-white text-sm">Sign up and get 20% off to your first order.
+                        <strong className="cursor-pointer"> Sign Up Now</strong></span>
+                    <span className="text-white cursor-pointer"><XMarkIcon className="h-6 w-6" /></span>
+                </div>
+            </header>
+            <header className="bg-white shadow-md stycky top-0 z-50">
+                <div className="container mx-auto flex justify-between items-center p-4">
+                    <a className="text-xl font-bold text-black cursor-pointer">DarkShop</a>
+                    <nav className="flex items-center space-x-6">
+                        <a className="hover:text-grey-700 cursor-pointer">Cover</a>
+                        <a className="hover:text-grey-700 cursor-pointer">Shop</a>
+                    </nav>
+                    <div className="mx-8">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <a className="cursor-pointer"><ShoppingCartIcon className="h-6 w-6 text-gray-700" /></a>
+                        <a className="cursor-pointer"><UserCircleIcon className="h-6 w-6 text-gray-700" /></a>
+                    </div>
+                </div>
+            </header>
+        </>
+
+    )
+}
