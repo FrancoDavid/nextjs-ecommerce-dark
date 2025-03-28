3785686b4d05cd312719a20a4abc7b11444a7931
redrum <franco.avalos@live.com>
2025-03-28 11:26:12 -0300

    feat: categories component layout

--- src/components/layouts/Categories.jsx
+++ src/components/layouts/Categories.jsx
@@ -3,29 +3,24 @@
 export default function Categories() {
     return (
         <div className="grid grid-cols-1 py-[80px] px-[100px]">
-            <div className="bg-gray-200 py-[70px] px-[64px] grid grid-cols-1 rounded-md">
+            <div className="bg-gray-200 py-[70px] px-[64px] grid grid-cols-1 rounded-[40px]">
                 <div className="flex justify-center items-center">
                     <h3 className="text-black font-bold text-4xl mb-[64px]">
-                        BROWSE BY dress STYLE
+                        BROWSE BY DRESS STYLE
                     </h3>
                 </div>
-                <div className="grid grid-cols-3 gap-5">
-                    <div className="p-4 w-full overflow-hidden rounded-md ">
-                        <h4>Casual</h4>
-                        <img className="object-cover" src='/imgs/image-11.png' alt={name} />
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="p-4 h-[280px] bg-white bg-[url('/imgs/image-11.png')] bg-cover bg-center rounded-[20px]">
+                        <h4 className="font-bold text-4xl">Casual</h4>
                     </div>
-                    <div className="p-4 col-span-2">
-                        <h4>Formal</h4>
-                        <img className="object-cover" src='/imgs/image-12.png'alt={name} />
+                    <div className="p-4 h-[280px] col-span-2 bg-white bg-[url('/imgs/image-12.png')] bg-cover bg-center rounded-[20px]">
+                        <h4 className="font-bold text-4xl">Formal</h4>
                     </div>
-                    <div className="p-4 col-span-2">
-                        <h4>Party</h4>
-                        <img className="object-cover" src='/imgs/image-13.png' alt={name} />
-
+                    <div className="p-4 h-[280px] col-span-2 bg-white bg-[url('/imgs/image-13.png')] bg-cover bg-center rounded-[20px]">
+                        <h4 className="font-bold text-4xl">Party</h4>
                     </div>
-                    <div className="p-4">
-                        <h4>Gym</h4>
-                        <img className="object-cover" src='/imgs/image-13.png' alt={name} />
+                    <div className="p-4 h-[280px] bg-white bg-[url('/imgs/image-14.png')] bg-cover bg-center rounded-[20px]">
+                        <h4 className="font-bold text-4xl">Gym</h4>
                     </div>
                 </div>
             </div>
