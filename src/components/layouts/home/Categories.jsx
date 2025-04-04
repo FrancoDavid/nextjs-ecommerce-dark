0f93f005243929cb14eee8140bb8ed6c24cd0d8c
redrum <franco.avalos@live.com>
2025-04-04 12:20:51 -0300

    feat: server componente categories

--- src/components/layouts/home/Categories.jsx
+++ src/components/layouts/home/Categories.jsx
@@ -1,19 +1,31 @@
-"use client"
+// "use client"
 
-import { useScroll } from "@/contexts/ScrollView";
+import { getCategories } from "@/services/Category"
 
-export default function Categories() {
-    const { categories } = useScroll();
+//  import { useScroll } from "@/contexts/ScrollView";
+
+export default async function Categories() {
+    // const { categories } = useScroll();
+
+    const categoriesResponse = await getCategories();
 
     return (
         <div className="grid grid-cols-1 py-[80px] px-[100px]">
-            <div className="bg-gray-200 py-[70px] px-[64px] grid grid-cols-1 rounded-[40px]" ref={categories}>
+            <div className="bg-gray-200 py-[70px] px-[64px] grid grid-cols-1 rounded-[40px]">
                 <div className="flex justify-center items-center">
                     <h3 className="text-black font-bold text-5xl mb-[64px]">
                         BROWSE BY DRESS STYLE
                     </h3>
                 </div>
                 <div className="grid grid-cols-3 gap-4">
+                    {categoriesResponse?.data?.map((category, index) => (
+                        <div className="p-4 h-[280px] bg-white bg-[url('/imgs/image-11.png')] bg-cover bg-center rounded-[20px] object-cover">
+                            <h4 className="font-bold text-4xl">{category?.name}</h4>
+                        </div>
+                    ))}
+                </div>
+
+                {/* <div className="grid grid-cols-3 gap-4">
                     <div className="p-4 h-[280px] bg-white bg-[url('/imgs/image-11.png')] bg-cover bg-center rounded-[20px] object-cover">
                         <h4 className="font-bold text-4xl">Casual</h4>
                     </div>
@@ -26,7 +38,7 @@ export default function Categories() {
                     <div className="p-4 h-[280px] bg-white bg-[url('/imgs/image-14.png')] bg-cover bg-center rounded-[20px] object-cover">
                         <h4 className="font-bold text-4xl">Gym</h4>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
