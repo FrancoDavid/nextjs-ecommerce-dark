812da77b8e299d32acb8a1941eda0da7f02762b8
redrum <franco.avalos@live.com>
2025-04-10 17:47:26 -0400

    feat: clean filters

--- src/components/layouts/Products/CategoriesFilter.jsx
+++ src/components/layouts/Products/CategoriesFilter.jsx
@@ -1,12 +1,16 @@
 "use client"
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChevronRightIcon } from "@heroicons/react/24/outline"
+import { ChevronRightIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import { useMemo } from "react";
 
 export default function CategoriesFilter({ name }) {
     const router = useRouter();
     const searchParams = useSearchParams();
     const pathname = usePathname();
+
+    const categoryName = useMemo(() => searchParams.get('categoryName'), [searchParams]);
+    const isSelected = useMemo(() => name === categoryName, [name, categoryName]);
 
     const handleFilterByCategory = (name) => () => {
         const params = new URLSearchParams(searchParams);
@@ -14,10 +18,19 @@ export default function CategoriesFilter({ name }) {
         router.push(`${pathname}?${params.toString()}`);
     }
 
+    const handleCleanFilterByCategory = () => {
+        const params = new URLSearchParams(searchParams);
+        params.delete('categoryName');
+        router.push(`${pathname}?${params.toString()}`);
+    }
+
     return (
-        <div className="flex items-center justify-between cursor-pointer" onClick={handleFilterByCategory(name)}>
-            <p className="text-[16px] font-light text-gray-400 hover:text-gray-600">{name}</p>
-            <ChevronRightIcon className="h-4 w-4 text-gray-700" />
+        <div className="flex items-center justify-between">
+            <p className={`text-[16px] font-light cursor-pointer ${isSelected? 'text-black' : 'text-gray-400'} hover:text-gray-600`} onClick={handleFilterByCategory(name)}>{name}</p>
+
+            {isSelected && (<XMarkIcon className="h-4 w-4 text-gray-700 cursor-pointer" onClick={handleCleanFilterByCategory} />)}
+
+            {!isSelected && (<ChevronRightIcon className="h-4 w-4 text-gray-700 cursor-pointer" onClick={handleFilterByCategory(name)}/>)}
         </div>
     )
 }
